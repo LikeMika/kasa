c536af46edf6d71546d13d534b8aa871d7c0fe14
Michael Bricout <bricout.michael@gmail.com>
2025-06-02 15:31:30 +0200

    Réalisation des tags

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,5 +1,6 @@
 import { useParams, Navigate } from 'react-router-dom';
 import Carousel from '../components/Carousel';
+import Tags from '../components/Tags';
 import logements from '../assets/data/logements.json';
 import '../styles/Logement.scss';
 
@@ -14,9 +15,23 @@ function Logement() {
   return (
     <div className="logement">
       <Carousel pictures={logement.pictures} />
-      {
-        <h1>{logement.title}</h1>
-      }
+      <div className='logement-wrapper'>
+        <div className='logement-info'>
+            <h1>{logement.title}</h1>
+            <h3>{logement.location}</h3>
+            <Tags tags={logement.tags} />
+        </div>
+        <div className='logement-host-rating'>
+            <div className='host'>
+                <div className='host-name'>
+                    {logement.host.name}
+                </div>
+                <div className='host-picture'>
+                     <img src={logement.host.picture} alt={logement.host.name} />
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
