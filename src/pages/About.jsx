48f18c3dd64de3efc4a22f64e7ad59c3b069d864
Michael Bricout <bricout.michael@gmail.com>
2025-06-04 01:11:42 +0200

    Texte du A propos dans un json

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,25 +1,19 @@
 import bannerAbout from '../assets/img/banner-about.png';
 import Banniere from '../components/Banniere';
 import Collapse from '../components/Collapse';
+import aboutData from '../assets/data/about.json';
 import '../styles/About.scss';
 
 function About() {
   return (
     <div className="about">
       <Banniere image={bannerAbout} noOverlay />
-      <div className="about-content">
-        <Collapse title="Fiabilité">
-          Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.
-        </Collapse>
-        <Collapse title="Respect">
-          La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.
-        </Collapse>
-        <Collapse title="Service">
-         La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.
-        </Collapse>
-        <Collapse title="Sécurité">
-          La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.
-        </Collapse>
+       <div className="about-content">
+        {aboutData.map((item, index) => (
+          <Collapse key={index} title={item.title}>
+            {item.content}
+          </Collapse>
+        ))}
       </div>
     </div>
   );
